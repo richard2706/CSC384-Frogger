a70297b0f3a15f2237603dfecb9c4a5c84b11dfe
Richard Wakeham <rwakeham27@outlook.com>
2022-02-13 21:45:26 +0000

    Implement basic car movement

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CarMovement : MonoBehaviour
+{
+    [SerializeField] private float speedMultiplier;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Move the car forwards at constant rate
+        Vector2 forwardVector = new Vector2(-transform.right.x, -transform.right.y); // Vector in direction of sprite
+        rb.MovePosition(rb.position + forwardVector * speedMultiplier * Time.fixedDeltaTime);
+    }
+}
