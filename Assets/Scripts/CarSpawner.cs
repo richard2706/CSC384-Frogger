3c701d2a24cbb021497bf974ef34b06e3b5dd803
Richard Wakeham <rwakeham27@outlook.com>
2022-05-10 00:08:58 +0100

    Add spawn direction toggle to carspawner

--- Assets/Scripts/CarSpawner.cs
+++ Assets/Scripts/CarSpawner.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class CarSpawner : MonoBehaviour
 {
-    [SerializeField] private float spawnInterval = 0.3f; // Length of time between car spawns.
+    [SerializeField] private float spawnInterval = 2f; // Length of time between car spawns.
     [SerializeField] private GameObject car; // Car prefab from which to spawn new cars.
+    [SerializeField] private bool spawnDirectionLeft; // If true, spawned cars move left, otherwise they move right.
     private Transform spawnPoint; // Location to spawn cars from.
     private float timeToNextSpawn = 0f; // Time until another car is spawned.
 
     private void Awake()
     {
+        // adjust spawnDirection based on serialzefield option or towards centre of road
         spawnPoint = transform;
+        if (spawnDirectionLeft) spawnPoint.Rotate(0f, 0f, 180f);
     }
 
     private void Update()
