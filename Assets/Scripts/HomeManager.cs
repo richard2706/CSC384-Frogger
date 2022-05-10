32b1c39d978421bff717333b7e2d528116a27c80
Richard Wakeham <rwakeham27@outlook.com>
2022-05-10 11:32:37 +0100

    Check for all homes filled

---
+++ Assets/Scripts/HomeManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class HomeManager : MonoBehaviour
+{
+    private static int totalHomes = 0;
+    private static int filledHomes = 0;
+
+    private bool homeFilled = false;
+
+    private void OnEnable()
+    {
+        totalHomes++;
+    }
+
+    private void OnDisable()
+    {
+        totalHomes--;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (!homeFilled && collider.GetComponentInParent<PlayerMovement>())
+        {
+            Debug.Log("Player reached a home");
+            homeFilled = true;
+            filledHomes++;
+            if (filledHomes == totalHomes)
+            {
+                Debug.Log("All homes filled");
+            }
+        }
+    }
+}
