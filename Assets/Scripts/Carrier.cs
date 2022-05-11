6869f21e2bcabc09c8ee0d3b842f20ef047e1923
Richard Wakeham <rwakeham27@outlook.com>
2022-05-11 19:44:38 +0100

    Implement player move off log

--- Assets/Scripts/Carrier.cs
+++ Assets/Scripts/Carrier.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class Carrier : MonoBehaviour
 {
-    public static event Action<Carrier> OnCarryableEnter;
-    //public static event Action<Carrier> OnCarryableLeave;
-
     private bool canCarry = true;
+    private Transform carrierTransform;
+
+    private void Awake()
+    {
+        carrierTransform = transform;
+    }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (canCarry && collider.GetComponentInParent<Carryable>())
         {
-            Debug.Log("Carryable touched a carrier");
-            OnCarryableEnter?.Invoke(this);
+            collider.transform.parent = carrierTransform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.GetComponentInParent<Carryable>())
+        {
+            collider.transform.parent = null;
         }
     }
 }
