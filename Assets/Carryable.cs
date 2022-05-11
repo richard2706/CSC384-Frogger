3db7216ada635291434a8a785aaa316a00273daa
Richard Wakeham <rwakeham27@outlook.com>
2022-05-11 19:27:01 +0100

    Implement player move with log

--- Assets/Carryable.cs
+++ Assets/Carryable.cs
@@ -1,6 +1,23 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class Carryable : MonoBehaviour
 {
-    
+    private bool beingCarried = false;
+
+    private void OnEnable()
+    {
+        Carrier.OnCarryableEnter += MoveWithCarrier;
+    }
+
+    private void OnDisable()
+    {
+        Carrier.OnCarryableEnter -= MoveWithCarrier;
+    }
+
+    private void MoveWithCarrier(Carrier carrier)
+    {
+        gameObject.transform.parent = carrier.transform;
+        beingCarried = true;
+    }
 }
--- Assets/Scripts/Carrier.cs
+++ Assets/Scripts/Carrier.cs
@@ -1,15 +1,20 @@
 using System;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class Carrier : MonoBehaviour
 {
+    public static event Action<Carrier> OnCarryableEnter;
+    //public static event Action<Carrier> OnCarryableLeave;
+
     private bool canCarry = true;
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.GetComponentInParent<Carryable>())
+        if (canCarry && collider.GetComponentInParent<Carryable>())
         {
             Debug.Log("Carryable touched a carrier");
+            OnCarryableEnter?.Invoke(this);
         }
     }
 }
