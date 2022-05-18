f174b3802cf66e506e87c7e20d906886c83f4b5b
Richard Wakeham <rwakeham27@outlook.com>
2022-05-18 14:26:37 +0100

    Start game on key pressed

---
+++ Assets/GameStateManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+    [SerializeField] GameObject startLevelPanel;
+    [SerializeField] GameObject winLevelPanel;
+    [SerializeField] GameObject loseLevelPanel;
+
+    private PlayerManager[] players;
+    private Spawner[] spawners;
+
+    private void Awake()
+    {
+        players = FindObjectsOfType<PlayerManager>(true);
+        spawners = FindObjectsOfType<Spawner>(true);
+    }
+
+    private void Start()
+    {
+        startLevelPanel.SetActive(true);
+        winLevelPanel.SetActive(false);
+        loseLevelPanel.SetActive(false);
+
+        DisableAll(players);
+        DisableAll(spawners);
+
+        StartCoroutine(WaitForStart());
+    }
+
+    private IEnumerator WaitForStart()
+    {
+        bool playerIndicatedStart = false;
+        while (!playerIndicatedStart)
+        {
+            if (Input.anyKey)
+            {
+                playerIndicatedStart = true;
+                StartLevel();
+            }
+            yield return null;
+        }
+    }
+
+    private void StartLevel()
+    {
+        startLevelPanel.SetActive(false);
+        EnableAll(players);
+        EnableAll(spawners);
+    }
+
+    private void EnableAll(MonoBehaviour[] objectBehaviours)
+    {
+        foreach (MonoBehaviour behaviour in objectBehaviours)
+        {
+            behaviour.gameObject.SetActive(true);
+        }
+    }
+
+    private void DisableAll(MonoBehaviour[] objectBehaviours)
+    {
+        foreach (MonoBehaviour behaviour in objectBehaviours)
+        {
+            behaviour.gameObject.SetActive(false);
+        }
+    }
+}
