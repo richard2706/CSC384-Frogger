a6898031502c69cdb1a7a9cae88822bee3997446
Richard Wakeham <rwakeham27@outlook.com>
2022-05-18 09:57:04 +0100

    Add menu button event handlers

---
+++ Assets/MenuButtonHandler.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class MenuButtonHandler : MonoBehaviour
+{
+    public void ContinueGame()
+    {
+        Debug.Log("Continue game");
+    }
+
+    public void RestartGame()
+    {
+        Debug.Log("Restart game");
+    }
+
+    public void ShowChangeProfilePanel()
+    {
+        Debug.Log("Change profile");
+    }
+
+    public void StartTwoPlayerGame()
+    {
+        Debug.Log("Two player game");
+    }
+
+    public void ShowAchievementsPanel()
+    {
+        Debug.Log("Achievements");
+    }
+
+    public void ShowCreditsPanel()
+    {
+        Debug.Log("Credits");
+    }
+}
