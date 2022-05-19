d03ae912674e9789c29fb910ef3d0fff5826fee7
Richard Wakeham <rwakeham27@outlook.com>
2022-05-19 15:54:30 +0100

    Set extra life as auto used

--- Assets/ExtraLifePowerUp.cs
+++ Assets/ExtraLifePowerUp.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public class ExtraLifePowerUp : PowerUp
 {
-    
+    public override void OnPickUp()
+    {
+        base.OnPickUp();
+        Debug.Log("Extra life picked up");
+    }
+
+    private void Start()
+    {
+        AutoUsed = true;
+    }
 }
